f8b1df88b9f4ba454bd3a73ff5e5d9af78e335f7
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2015-10-21 16:02:08 +0200

    add check dependencies icons
     and fix responsive sidebar behaviour and fix indentation

--- app/controllers/steps.js
+++ app/controllers/steps.js
@@ -1,5 +1,7 @@
 app.controller('stepsCtrl', function($scope, $routeParams, StepsFactory, StepsDescriptionFactory) {
 
+    $scope.steps = [];
+
     function findItem(arr, key, value) {
         for (var i = 0; i < arr.length; i++) {
             if (arr[i][key] === value) {
@@ -7,6 +9,25 @@ app.controller('stepsCtrl', function($scope, $routeParams, StepsFactory, StepsDe
             }
         }
         return -1;
+    }
+
+    function checkDependencies(steps) {
+        if (steps.length > 0) {
+            steps.forEach(function(step){
+                if (step.command && step.command.length > 0) {
+                    $('#sidebar a[data-id="'+step.number+'"]').removeClass('success').removeClass('error');
+                    var message = {
+                        step : step,
+                        type: $scope.currentType,
+                    };
+                    studio.sendCommand('troubleShooting.getTroubleshootingDependencyCheck.'+btoa(JSON.stringify(message))); 
+                } else {
+                    setTimeout(function(){
+                        $('#sidebar a[data-id="'+step.number+'"]').addClass('loaded');
+                    },500);
+                }
+            })
+        }
     }
 
     var currentStepPosition = 1;
@@ -17,7 +38,6 @@ app.controller('stepsCtrl', function($scope, $routeParams, StepsFactory, StepsDe
 
     StepsFactory.getSteps({
         id: $routeParams.id
-        
     }).$promise.then(function(res) {
         $scope.currentType = res.__ENTITIES[0];
         $scope.currentStep = $scope.currentType.steps.__ENTITIES[findItem($scope.currentType.steps.__ENTITIES, "number", currentStepPosition)];
@@ -26,7 +46,9 @@ app.controller('stepsCtrl', function($scope, $routeParams, StepsFactory, StepsDe
             id: $scope.currentStep.ID
         }).$promise.then(function(res) {
             $scope.descriptions = res.__ENTITIES[0].description.__ENTITIES;
+            checkDependencies($scope.currentType.steps.__ENTITIES);
         });
+        
     });
 
     $scope.setCurrentStep = function(step) {
@@ -35,10 +57,25 @@ app.controller('stepsCtrl', function($scope, $routeParams, StepsFactory, StepsDe
         StepsDescriptionFactory.getDescription({
             id: $scope.currentStep.ID
         }).$promise.then(function(res) {
+            checkDependencies([$scope.currentStep]);
             $scope.descriptions = res.__ENTITIES[0].description.__ENTITIES;
         });
     };
 });
+
+app.updateStepDependency = function(type, step, result) {
+    setTimeout(function(){
+        if (result == true) {
+            $('#sidebar a[data-id="'+step+'"]').addClass('success');
+        } else if (result == false) {
+            $('#sidebar a[data-id="'+step+'"]').addClass('error');        
+        } else {
+            if (!$('#sidebar a[data-id="'+step+'"]').hasClass('success')) {
+                $('#sidebar a[data-id="'+step+'"]').addClass('error');
+            }
+        }
+    },500);
+}
 
 app.factory('StepsFactory', ['$resource', function($resource) {
     return $resource('http://troubleshooting.us.wak-apps.com/rest/ApplicationType/?$filter="ID =:id"&$expand=steps', null, {
--- index.js
+++ index.js
@@ -1,68 +1,73 @@
 /* Copyright (c) WAKANDA, 2015
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* The Software shall be used for Good, not Evil.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
-
-var
-	actions;
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * The Software shall be used for Good, not Evil.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+var utils = require("../wakanda-extension-mobile-core/utils");
+var actions;
 actions = {};
 
-actions.isWindows = function isWindows()
-{
+actions.isWindows = function isWindows() {
     return os.isWindows;
 }
 
-
-actions.getTroubleshootingPage = function getTroubleshootingPage()
-{
- 
+actions.getTroubleshootingPage = function getTroubleshootingPage() {
     studio.extension.registerTabPage("./index.html", './rsz_welcome.png');
     studio.extension.openPageInTab('./index.html', 'TroubleShooting Page', false);
 }
 
+actions.getTroubleshootingDependencyCheck = function getTroubleshootingDependencyCheck(message) {
+    var step = message.params.step;
+    var type = message.params.type;
+    var command = {
+        cmd: step.command,
+        onmessage: function(msg) {
+            studio.sendExtensionWebZoneCommand('troubleShooting','app.updateStepDependency',[type,step.number,true]);
+        },
+        onerror: function(msg) {
+            studio.sendExtensionWebZoneCommand('troubleShooting','app.updateStepDependency',[type,step.number,false]);
+        },
+        onterminated: function(msg) {
+            studio.sendExtensionWebZoneCommand('troubleShooting','app.updateStepDependency',[type,step.number,null]);
+        }
+    };
+    var worker = utils.executeAsyncCmd(command);
+}
 
-exports.handleMessage = function handleMessage(message)
-{
-	"use strict";
-	var	actionName;
-
-	actionName = message.action;
-
-	if (!actions.hasOwnProperty(actionName)) {
-		studio.alert("I don't know about this message: " + actionName);
-		return false;
-	}
-	actions[actionName](message);
-};
-
-actions.goToTroubleShootingStep = function goToTroubleShootingStep(message)
-{
-    if(message.params.nickname && message.params.step ){
-        
-    studio.extension.storage.setItem("nickname",message.params.nickname);
-    studio.extension.storage.setItem("step",message.params.step);
-    studio.sendExtensionWebZoneCommand('troubleShooting','location.reload');
-    
-    studio.extension.registerTabPage("./index.html", './rsz_welcome.png');
-    studio.extension.openPageInTab("./index.html", 'TroubleShooting Page', false);
-    
+actions.goToTroubleShootingStep = function goToTroubleShootingStep(message) {
+    if (message.params.nickname && message.params.step) {
+        studio.extension.storage.setItem("nickname", message.params.nickname);
+        studio.extension.storage.setItem("step", message.params.step);
+        studio.sendExtensionWebZoneCommand('troubleShooting', 'location.reload');
+        studio.extension.registerTabPage("./index.html", './rsz_welcome.png');
+        studio.extension.openPageInTab("./index.html", 'TroubleShooting Page', false);
     }
 }
+
+exports.handleMessage = function handleMessage(message) {
+    "use strict";
+    var actionName;
+    actionName = message.action;
+    if (!actions.hasOwnProperty(actionName)) {
+        studio.alert("I don't know about this message: " + actionName);
+        return false;
+    }
+    actions[actionName](message);
+};
