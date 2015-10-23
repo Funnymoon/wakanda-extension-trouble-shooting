ee5cc93567ec7c3650a67e478a058e1e326566a2
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2015-10-23 15:03:25 +0200

    change tab name

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ actions = {};
 
 actions.getTroubleshootingPage = function getTroubleshootingPage() {
     studio.extension.registerTabPage("./index.html", './rsz_welcome.png');
-    studio.extension.openPageInTab('./index.html', 'TroubleShooting Page', false);
+    studio.extension.openPageInTab('./index.html', 'TroubleShooting', false);
 }
 
 actions.getTroubleshootingDependencyCheck = function getTroubleshootingDependencyCheck(message) {
